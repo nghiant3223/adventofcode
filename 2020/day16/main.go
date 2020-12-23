1cf9c4b5d74ec651f361e17030845208def535a1
nghiant11 <nghiant11@vng.com.vn>
2020-12-23 10:56:20 +0700

    refactor day 16

--- 2020/day16/main.go
+++ 2020/day16/main.go
@@ -67,54 +67,55 @@ func part1(rules []Rule, tickets []Ticket) int {
 
 func part2(rules []Rule, tickets []Ticket) []Rule {
 	currentIndex := 0
-	columnPossibleRules := getColumnPossibleRules(rules, tickets)
-	columnVisitedOrder := sortColumnByRuleSpace(columnPossibleRules)
+	columnRuleSpaces := getColumnRuleSpaces(rules, tickets)
+	columnVisitedOrder := sortColumnByRuleSpace(columnRuleSpaces)
 	ruleOrder := make([]Rule, len(rules))
 	usedRule := make(map[Rule]struct{}, len(rules))
-	successful := backtrack(columnVisitedOrder, currentIndex, columnPossibleRules, ruleOrder, usedRule)
+	successful := backtrack(columnVisitedOrder, currentIndex, columnRuleSpaces, ruleOrder, usedRule)
 	if !successful {
 		return nil
 	}
 	return ruleOrder
 }
 
-func getColumnPossibleRules(rules []Rule, tickets []Ticket) map[int][]Rule {
+func getColumnRuleSpaces(rules []Rule, tickets []Ticket) map[int][]Rule {
+	ruleCount := len(rules)
 	columnCount := len(rules)
 	ticketCount := len(tickets)
-	columnPossibleRules := make(map[int][]Rule)
+	columnRuleSpace := make(map[int][]Rule)
 	for column := 0; column < columnCount; column++ {
 		fieldsInColumn := make([]int, ticketCount)
 		for i, ticket := range tickets {
 			fieldsInColumn[i] = ticket.Fields[column]
 		}
-		var possibleRules []Rule
+		possibleRules := make([]Rule, 0, ruleCount)
 		for _, rule := range rules {
 			if rule.Validate(fieldsInColumn) {
 				possibleRules = append(possibleRules, rule)
 			}
 		}
-		columnPossibleRules[column] = possibleRules
+		columnRuleSpace[column] = possibleRules
 	}
-	return columnPossibleRules
+	return columnRuleSpace
 }
 
 func backtrack(
 	columnVisitOrder []int, currentVisitedColumnIndex int,
-	columnPossibleRules map[int][]Rule, ruleOrder []Rule, ruleUsed map[Rule]struct{},
+	columnRuleSpace map[int][]Rule, ruleOrder []Rule, ruleUsed map[Rule]struct{},
 ) bool {
 	if currentVisitedColumnIndex >= len(columnVisitOrder) {
 		return true
 	}
 	currentVisitedColumn := columnVisitOrder[currentVisitedColumnIndex]
-	for _, possibleRule := range columnPossibleRules[currentVisitedColumn] {
-		_, used := ruleUsed[possibleRule]
+	for _, rule := range columnRuleSpace[currentVisitedColumn] {
+		_, used := ruleUsed[rule]
 		if used {
 			continue
 		}
-		ruleUsed[possibleRule] = struct{}{}
-		ruleOrder[currentVisitedColumn] = possibleRule
-		successful := backtrack(columnVisitOrder, currentVisitedColumnIndex+1, columnPossibleRules, ruleOrder, ruleUsed)
-		delete(ruleUsed, possibleRule)
+		ruleUsed[rule] = struct{}{}
+		ruleOrder[currentVisitedColumn] = rule
+		successful := backtrack(columnVisitOrder, currentVisitedColumnIndex+1, columnRuleSpace, ruleOrder, ruleUsed)
+		delete(ruleUsed, rule)
 		if successful {
 			return true
 		}
@@ -122,16 +123,16 @@ func backtrack(
 	return false
 }
 
-func sortColumnByRuleSpace(columnPossibleRules map[int][]Rule) []int {
-	columnCount := len(columnPossibleRules)
+func sortColumnByRuleSpace(columnRuleSpace map[int][]Rule) []int {
+	columnCount := len(columnRuleSpace)
 	column := make([]int, columnCount)
 	for i := 0; i < columnCount; i++ {
 		column[i] = i
 	}
 	// sort column by rule space ascending
 	sort.Slice(column, func(i, j int) bool {
-		columnIRuleSpace := columnPossibleRules[column[i]]
-		columnJRuleSpace := columnPossibleRules[column[j]]
+		columnIRuleSpace := columnRuleSpace[column[i]]
+		columnJRuleSpace := columnRuleSpace[column[j]]
 		return len(columnIRuleSpace) < len(columnJRuleSpace)
 	})
 	return column
